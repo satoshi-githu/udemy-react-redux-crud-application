6a87c48a38ecdd5c8d66bec8dd676d76f6145944
satoshi-githu <rie1366@yahoo.co.jp>
2021-10-29 23:13:03 +0900

    lean props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 const App = () => {
+  const  profiles = [
+    { name : 'Taro', age: 10},
+    { name : 'Hanako', age: 5},
+    { name : 'satoshi'}
+  ]
   return (
     <>
-    <Cat />
-    <Cat />
-    <Cat />
-    <Cat />
-    <Cat />
-
+    {profiles.map((profile, index) => {
+      return <User name={profile.name} age={profile.age} key={index}/>
+    })}
     </>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}! I am {props.age} years old! </div>
+}
 
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
